7624d6fa1381d95bf3add0dd2a876e3d67b26be7
braulio <deeppage0603@gmail.com>
2024-05-10 20:09:40 -0600

    add zip

---
+++ zip.py
@@ -0,0 +1,12 @@
+import shutil
+import os
+
+def zip_folder(folder_path, output_path):
+    base_dir = os.path.basename(folder_path)
+    parent_dir = os.path.dirname(folder_path)
+
+    shutil.make_archive(output_path, 'zip', parent_dir, base_dir)
+
+    print(f'Folder "{folder_path}" has been zipped as "{output_path}.zip"')
+
+zip_folder('download', 'download.zip')
